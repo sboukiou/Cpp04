486b47f7d94369cbb3297ea0c866ab51bbd627d6
sboukiou <sboukiou@student.1337.ma>
2025-12-25 00:28:34 +0100

    Added tests for Cat and Dog

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,4 +1,6 @@
 #include "./Animal.hpp"
+#include "./Cat.hpp"
+#include "./Dog.hpp"
 #include <iostream>
 
 void	testAnimalClass(void) {
@@ -20,7 +22,51 @@ void	testAnimalClass(void) {
 	std::cout << "----------------\n\n";
 }
 
+void	testCatClass(void) {
+	std::cout << "----------------\n";
+	std::cout <<  "Testing the Cat Class\n";
+	std::cout << "----------------\n";
+	Animal *unknown = new Cat;
+	Animal *Dinosaur = new Cat("T-rex");
+	Animal *ChildDino = new Cat(*Dinosaur);
+
+	ChildDino->setType("Mini-T-Rex");
+
+	unknown->makeSound();
+	Dinosaur->makeSound();
+	ChildDino->makeSound();
+	delete unknown;
+	delete Dinosaur;
+	delete ChildDino;
+	std::cout << "----------------\n\n";
+}
+
+void	testDogClass(void) {
+	std::cout << "----------------\n";
+	std::cout <<  "Testing the Dog Class\n";
+	std::cout << "----------------\n";
+	Animal *unknown = new Dog;
+	Animal *Dinosaur = new Dog("T-rex");
+	Animal *ChildDino = new Dog(*Dinosaur);
+
+	ChildDino->setType("Mini-T-Rex");
+
+	unknown->makeSound();
+	Dinosaur->makeSound();
+	ChildDino->makeSound();
+	delete unknown;
+	delete Dinosaur;
+	delete ChildDino;
+	std::cout << "----------------\n\n";
+}
+
+
 int	main(void) {
 	testAnimalClass();
+	std::cout << "----------------\n\n";
+	testCatClass();
+	std::cout << "----------------\n\n";
+	testDogClass();
+	std::cout << "----------------\n\n";
 	return (0);
 }
