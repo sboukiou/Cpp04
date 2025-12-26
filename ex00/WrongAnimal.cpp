e61c1da8f979c1e930ecc93cadb848c5f5569b7e
sboukiou <sboukiou@student.1337.ma>
2025-12-26 17:40:25 +0100

    Added implementation for wrong animals

---
+++ ex00/WrongAnimal.cpp
@@ -0,0 +1,61 @@
+#include "./WrongAnimal.hpp"
+#include <iostream>
+#include <cstdlib>
+
+WrongAnimal::WrongAnimal() {
+	type = "(WrongAnimal)";
+	std::cout << "[INFO]:(Default Construction){WrongAnimal}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+WrongAnimal::WrongAnimal(const std::string &param) {
+	type = param;
+	std::cout << "[INFO]:(Parameterized Construction){WrongAnimal}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal &param) {
+	type = param.getType();
+	std::cout << "[INFO]:(Copy Construction){WrongAnimal}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+
+WrongAnimal::~WrongAnimal(void) {
+	std::cout << "[INFO]:(Destruction){WrongAnimal}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+
+void	WrongAnimal::operator=(const WrongAnimal &param) {
+	type = param.getType();
+	std::cout << "[INFO]:( <=> Operator overloading){WrongAnimal}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+
+const std::string	WrongAnimal::getType(void) const{
+	return (type);
+}
+void	WrongAnimal::setType(const std::string &param) {
+	type = param;
+}
+
+void	WrongAnimal::makeSound(void) const {
+	std::cout << "[INFO]:(Make Sound){Default WrongAnimal wont make any}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
---
+++ ex00/WrongCat.cpp
@@ -0,0 +1,54 @@
+#include "./WrongCat.hpp"
+#include <iostream>
+#include <cstdlib>
+
+WrongCat::WrongCat() {
+	type = "(WrongCat)";
+	std::cout << "[INFO]:(Default Construction){WrongCat}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+WrongCat::WrongCat(const std::string &param) {
+	type = param;
+	std::cout << "[INFO]:(Parameterized Construction){WrongCat}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+
+WrongCat::WrongCat(const WrongAnimal &param) {
+	type = param.getType();
+	std::cout << "[INFO]:(Copy Construction){WrongCat}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+
+WrongCat::~WrongCat(void) {
+	std::cout << "[INFO]:(Destruction){WrongCat}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+
+void	WrongCat::operator=(const WrongCat &param) {
+	type = param.getType();
+	std::cout << "[INFO]:( <=> Operator overloading){WrongCat}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
+
+void	WrongCat::makeSound(void) const {
+	std::cout << "[INFO]:(WrongCat Make Sound){Meowwwwww !}\n";
+	if (std::cout.fail()) {
+		std::cerr << "[ERR]: (Failed to write to stdout)\n";
+		exit(0);
+	}
+}
